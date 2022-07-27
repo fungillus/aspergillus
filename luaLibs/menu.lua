481a66b10abad364f603a362ca1686e608ab446c
Nicholas Niro <blowfist@xroutine.net>
2022-07-27 15:17:46 -0400

    Implemented the new menu lua library and converted menu/main.lua to it.

---
+++ luaLibs/menu.lua
@@ -0,0 +1,239 @@
+
+require("basic2")
+require("fonts")
+
+require("convertImage")
+
+Menu = {drawContext = nil, entries = nil, drawBitmaps = nil, currentPage = nil, coordinate = nil, arrowBitmaps = nil, alignment = nil}
+
+function Menu:new(drawContext, coordinate, menuEntries)
+	o = o or {}
+	o.drawContext = drawContext
+	o.entries = menuEntries or {}
+	o.drawnMenuObjects = {}
+	o.coordinate = {x = coordinate.x, y = coordinate.y} or {x = 0, y = 0}
+	o.currentPage = 0
+	o.alignment = "left"
+	o.arrowBitmaps = {}
+
+	o.arrowBitmaps.up = convertRawTextToImage([[
+   *
+  ***
+ *****
+*******
+  ***
+  ***
+]])
+
+	o.arrowBitmaps.down = convertRawTextToImage([[
+  ***
+  ***
+*******
+ *****
+  ***
+   *
+]])
+
+	o.arrowBitmaps.right = convertRawTextToImage([[
+  *
+  **
+*****
+******
+*****
+  **
+  *
+]])
+
+	o.arrowBitmaps.left = convertRawTextToImage([[
+   *
+  **
+ *****
+******
+ *****
+  **
+   *
+]])
+
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function Menu:clean()
+	if #self.drawnMenuObjects > 0 then
+		for i, drawnMenuObject in pairs(self.drawnMenuObjects) do
+			self.drawContext.screen:blitReverse(drawnMenuObject.bitmap, drawnMenuObject.coordinate.x, drawnMenuObject.coordinate.y)
+		end
+
+		self.drawnMenuObjects = {}
+	end
+end
+
+function Menu:draw()
+	if #self.drawnMenuObjects > 0 then
+		self:clean()
+	end
+
+	self:_populateMenuEntries()
+	for i, drawnMenuObject in pairs(self.drawnMenuObjects) do
+		self.drawContext.screen:blit(drawnMenuObject.bitmap, drawnMenuObject.coordinate.x, drawnMenuObject.coordinate.y)
+	end
+
+	self.drawContext.screen:draw()
+end
+
+function Menu:_populateMenuEntries()
+	local currentEntry = nil
+	local entryImg = nil
+	local baseBorderSize = 1
+	local borderSize = nil
+	local baseCoordinates = {x = self.coordinate.x, y = self.coordinate.y}
+	local maxXCoordinate = 0
+	local calculatedXCoordinate = 0
+
+	local isFirst = true
+	local alignment = self.alignment
+	for entryIndex, currentEntry in pairs(o.entries) do
+		if currentEntry.selected == true then
+			borderSize = baseBorderSize + 1
+		else
+			borderSize = baseBorderSize
+		end
+		if currentEntry.type == "button" then
+			entryImg = _createButton(self.drawContext.fonts, {drawBorder = true, borderSize = borderSize}, currentEntry.name)
+
+			if isFirst then
+				baseCoordinates.x = giveMiddlePoint(self.drawContext.screen, entryImg)
+				maxXCoordinate = baseCoordinates.x + entryImg.width
+			end
+
+			calculatedXCoordinate = alignmentCalculation(self.drawContext.screen, entryImg, baseCoordinates.x, maxXCoordinate, alignment)
+
+			table.insert(o.drawnMenuObjects, {bitmap = entryImg, coordinate = {x = calculatedXCoordinate, y = baseCoordinates.y}})
+
+			baseCoordinates.y = baseCoordinates.y + entryImg.height + 2
+
+			if isFirst then
+				isFirst = false
+			end
+		end
+	end
+end
+
+function Menu:alignCenter()
+	self.alignment = "centered"
+end
+
+function Menu:alignLeft()
+	self.alignment = "left"
+end
+
+function Menu:alignRight()
+	self.alignment = "right"
+end
+
+function _createButton(fonts, config, text)
+	config = config or {}
+
+	if config.drawBorder == nil then
+		config.drawBorder = false
+	end
+
+	if config.borderSize == nil then
+		config.borderSize = 1
+	end
+
+	local width = fonts:getCharacterWidth() * #text
+	local height = fonts:getCharacterHeight()
+
+	local textStartX = 0
+	local textStartY = 0
+
+	if config.drawBorder then
+		height = height + (config.borderSize * 2) + 2
+
+		width = width + (config.borderSize * 2) + 4
+
+		textStartX = config.borderSize + 2
+		textStartY = config.borderSize + 2
+	end
+
+	local bitmap = Bitmap:new(width, height)
+
+	fonts:printText(bitmap, {x=textStartX, y=textStartY}, text)
+
+	if config.drawBorder then
+		bitmap:drawBorder(config.borderSize)
+	end
+
+	return bitmap
+end
+
+function giveMiddlePoint(primaryBitmap, secondaryBitmap)
+	return (primaryBitmap.width / 2) - (secondaryBitmap.width / 2)
+end
+
+-- alignment is : centered, left or right
+function alignmentCalculation(screen, image, baseXCoordinate, maxXCoordinate, alignment)
+	local result = 0
+	if alignment == "centered" then
+		result = giveMiddlePoint(screen, image)
+	elseif alignment == "left" then
+		result = baseXCoordinate
+	elseif alignment == "right" then
+		-- the goal is : baseXCoordinate + x + image.width == maxXCoordinate
+		-- 		 x = maxXCoordinate - (baseXCoordinate + image.width)
+		result = baseXCoordinate + (maxXCoordinate - (baseXCoordinate + image.width))
+	else
+		result = alignmentCalculation(screen, image, "left")
+	end
+
+	return result
+end
+
+function Menu:selectNext()
+	local mustSelectNext = false
+	for entryIndex in pairs(self.entries) do
+		if mustSelectNext == true then
+			self.entries[entryIndex].selected = true
+			break
+		end
+		if self.entries[entryIndex].selected == true then
+			if entryIndex == #self.entries then -- can't go beyond the last entry element
+				break
+			end
+			self.entries[entryIndex].selected = nil
+			mustSelectNext = true
+		end
+	end
+	self:draw()
+end
+
+function Menu:selectPrevious()
+	local mustSelectNext = false
+	for entryIndex = #self.entries, 0, -1 do
+		if mustSelectNext == true then
+			self.entries[entryIndex].selected = true
+			break
+		end
+		if self.entries[entryIndex].selected == true then
+			if entryIndex == 0 then -- can't go before the first entry element
+				break
+			end
+			self.entries[entryIndex].selected = nil
+			mustSelectNext = true
+		end
+	end
+
+	self:draw()
+end
+
+function Menu:trigger()
+	for entryIndex, entry in pairs(self.entries) do
+		if entry.selected == true then
+			if entry.onTrigger ~= nil then
+				entry.onTrigger()
+			end
+		end
+	end
+end
--- menu/main.lua
+++ menu/main.lua
@@ -1,195 +1,10 @@
-
 require("basic2")
 require("fonts")
 require("console")
 
-require("convertImage")
+require("menu")
 
-function createButton(fonts, config, text)
-	config = config or {}
-
-	if config.drawBorder == nil then
-		config.drawBorder = false
-	end
-
-	if config.borderSize == nil then
-		config.borderSize = 1
-	end
-
-	local width = fonts:getCharacterWidth() * #text
-	local height = fonts:getCharacterHeight()
-
-	local textStartX = 0
-	local textStartY = 0
-
-	if config.drawBorder then
-		height = height + (config.borderSize * 2) + 2
-
-		width = width + (config.borderSize * 2) + 4
-
-		textStartX = config.borderSize + 2
-		textStartY = config.borderSize + 2
-	end
-
-	local bitmap = Bitmap:new(width, height)
-
-	fonts:printText(bitmap, {x=textStartX, y=textStartY}, text)
-
-	if config.drawBorder then
-		bitmap:drawBorder(config.borderSize)
-	end
-
-	return bitmap
-end
-
-function giveMiddlePoint(screen, image)
-	return (screen.width / 2) - (image.width / 2)
-end
-
--- alignment is : centered, left or right
-function alignmentCalculation(screen, image, baseXCoordinate, maxXCoordinate, alignment)
-	local result = 0
-	if alignment == "centered" then
-		result = giveMiddlePoint(screen, image)
-	elseif alignment == "left" then
-		result = baseXCoordinate
-	elseif alignment == "right" then
-		-- the goal is : baseXCoordinate + x + image.width == maxXCoordinate
-		-- 		 x = maxXCoordinate - (baseXCoordinate + image.width)
-		result = baseXCoordinate + (maxXCoordinate - (baseXCoordinate + image.width))
-	else
-		result = alignmentCalculation(screen, image, "left")
-	end
-
-	return result
-end
-
-function drawMenu(ctx, coordinates, menuData)
-	local currentEntry = nil
-	local entryImg = nil
-	local baseBorderSize = 1
-	local borderSize = nil
-	local baseCoordinates = {x = coordinates.x, y = coordinates.y}
-	local maxXCoordinate = 0
-	local calculatedXCoordinate = 0
-
-	local isFirst = true
-	local alignment = "left"
-	for entryIndex in pairs(menuData) do
-		currentEntry = menuData[entryIndex]
-		if currentEntry.selected == true then
-			borderSize = baseBorderSize + 1
-		else
-			borderSize = baseBorderSize
-		end
-		if currentEntry.type == "button" then
-			entryImg = createButton(ctx.fonts, {drawBorder = true, borderSize = borderSize}, currentEntry.name)
-
-			if isFirst then
-				baseCoordinates.x = giveMiddlePoint(ctx.screen, entryImg)
-				maxXCoordinate = baseCoordinates.x + entryImg.width
-			end
-
-			calculatedXCoordinate = alignmentCalculation(ctx.screen, entryImg, baseCoordinates.x, maxXCoordinate, alignment)
-
-			ctx.screen:blit(entryImg, calculatedXCoordinate, baseCoordinates.y)
-			baseCoordinates.y = baseCoordinates.y + entryImg.height + 2
-
-			if isFirst then
-				isFirst = false
-			end
-		end
-	end
-end
-
-function clearMenu(ctx, coordinates, menuData)
-	local currentEntry = nil
-	local entryImg = nil
-	local baseBorderSize = 1
-	local borderSize = nil
-	local baseCoordinates = {x = coordinates.x, y = coordinates.y}
-	local maxXCoordinate = 0
-	local calculatedXCoordinate = 0
-
-	local isFirst = true
-	local alignment = "left"
-	for entryIndex in pairs(menuData) do
-		currentEntry = menuData[entryIndex]
-		if currentEntry.selected == true then
-			borderSize = baseBorderSize + 1
-		else
-			borderSize = baseBorderSize
-		end
-		if currentEntry.type == "button" then
-			entryImg = createButton(ctx.fonts, {drawBorder = true, borderSize = borderSize}, currentEntry.name)
-
-			if isFirst then
-				baseCoordinates.x = giveMiddlePoint(ctx.screen, entryImg)
-				maxXCoordinate = baseCoordinates.x + entryImg.width
-			end
-
-			calculatedXCoordinate = alignmentCalculation(ctx.screen, entryImg, baseCoordinates.x, maxXCoordinate, alignment)
-
-			ctx.screen:blitReverse(entryImg, calculatedXCoordinate, baseCoordinates.y)
-			baseCoordinates.y = baseCoordinates.y + entryImg.height + 2
-
-			if isFirst then
-				isFirst = false
-			end
-		end
-	end
-end
-
-function selectNextMenuEntry(menuData)
-	local mustSelectNext = false
-	for entryIndex in pairs(menuData) do
-		if mustSelectNext == true then
-			menuData[entryIndex].selected = true
-			break
-		end
-		if menuData[entryIndex].selected == true then
-			if entryIndex == #menuData then -- can't go beyond the last entry element
-				break
-			end
-			menuData[entryIndex].selected = nil
-			mustSelectNext = true
-		end
-	end
-
-	return menuData
-end
-
-function selectPreviousMenuEntry(menuData)
-	local mustSelectNext = false
-	for entryIndex = #menuData, 0, -1 do
-		if mustSelectNext == true then
-			menuData[entryIndex].selected = true
-			break
-		end
-		if menuData[entryIndex].selected == true then
-			if entryIndex == 0 then -- can't go before the first entry element
-				break
-			end
-			menuData[entryIndex].selected = nil
-			mustSelectNext = true
-		end
-	end
-
-	return menuData
-end
-
-function triggerMenuEntry(menuData)
-	for entryIndex, entry in pairs(menuData) do
-		if entry.selected == true then
-			if entry.onTrigger ~= nil then
-				entry.onTrigger()
-			end
-		end
-	end
-end
-
-currentTick = 0
-menuContext = {}
+menuContext = nil
 
 function Init()
 	--print "\x1b[25l"
@@ -198,71 +13,23 @@ function Init()
 
 	local fonts = Fonts:new()
 
-	local ctx = {screen = screen, fonts = fonts}
-
-
-	local imgUpArrow = convertRawTextToImage([[
-   *
-  ***
- *****
-*******
-  ***
-  ***
-]])
-
-	local imgDownArrow = convertRawTextToImage([[
-  ***
-  ***
-*******
- *****
-  ***
-   *
-]])
-
-	local imgRightArrow = convertRawTextToImage([[
-  *
-  **
-*****
-******
-*****
-  **
-  *
-]])
-
-	local imgLeftArrow = convertRawTextToImage([[
-   *
-  **
- *****
-******
- *****
-  **
-   *
-]])
-
 	-- sound menu
 	-- input binding menu
 	-- video menu
 	--
 	-- menu item types : button, configToggle, configValueSlider, configValueSelector
-	menuContext.menuData = {
+	local menuData = {
 		{name = "Main Menu", type = "button", onTrigger = nil, selected = true}
 		,{name = "Settings", type = "button", onTrigger = nil}
 		,{name = "Quit", type = "button", onTrigger = quit}
 	}
 
-	menuContext.ctx = ctx
-	menuContext.baseMenuCoordinate = {x = 25, y = 20}
-
-	drawMenu(ctx, menuContext.baseMenuCoordinate, menuContext.menuData)
-
-	-- show the arrow images
-	screen:blit(imgUpArrow, 7, 2)
-	screen:blit(imgLeftArrow, 2, 7)
-	screen:blit(imgRightArrow, 13, 7)
-	screen:blit(imgDownArrow, 7, 13)
+	menuContext = Menu:new({screen = screen, fonts = fonts}, {x = 25, y = 20}, menuData)
+	menuContext:alignLeft()
 
 	screen:drawBorder(1)
-	screen:draw()
+
+	menuContext:draw()
 end
 
 function quit()
@@ -314,20 +81,12 @@ function handleInputs()
 	elseif currentButtons & buttons.kButtonRight == buttons.kButtonRight then
 		return
 	elseif currentButtons & buttons.kButtonUp == buttons.kButtonUp then
-		clearMenu(menuContext.ctx, menuContext.baseMenuCoordinate, menuContext.menuData)
-		menuContext.menuData = selectPreviousMenuEntry(menuContext.menuData)
-		drawMenu(menuContext.ctx, menuContext.baseMenuCoordinate, menuContext.menuData)
-		menuContext.ctx.screen:draw()
+		menuContext:selectPrevious()
 	elseif currentButtons & buttons.kButtonDown == buttons.kButtonDown then
-		clearMenu(menuContext.ctx, menuContext.baseMenuCoordinate, menuContext.menuData)
-		menuContext.menuData = selectNextMenuEntry(menuContext.menuData)
-		drawMenu(menuContext.ctx, menuContext.baseMenuCoordinate, menuContext.menuData)
-		menuContext.ctx.screen:draw()
+		menuContext:selectNext()
 	elseif currentButtons & buttons.kButtonA == buttons.kButtonA then
-		triggerMenuEntry(menuContext.menuData)
+		menuContext:trigger()
 	end
-
-
 end
 
 function Poll()
