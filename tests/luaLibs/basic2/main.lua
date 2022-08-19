d75318f4b46a5cb551666d7818fa963a3ad6ac06
Nicholas Niro <blowfist@xroutine.net>
2022-08-19 03:35:55 -0400

    Started to create tests with the basic2 library.

---
+++ tests/luaLibs/basic2/main.lua
@@ -0,0 +1,64 @@
+
+
+function Init()
+	local convRawUnicodeValueToUnicodeTests = {
+		{"first", convRawUnicodeValueToUnicode, {0x28FF}, "⣿"}
+		,{"second", convRawUnicodeValueToUnicode, {0x2807}, "⠇"}
+		,{"third", convRawUnicodeValueToUnicode, {0x2847}, "⡇"}
+		,{"fourth", convRawUnicodeValueToUnicode, {0x28E1}, "⣡"}
+		,{"fifth", convRawUnicodeValueToUnicode, {0x28F0}, "⣰"}
+	}
+
+	if not doTests("convRawUnicodeValueToUnicode", convRawUnicodeValueToUnicodeTests) then
+		return
+	end
+
+	-- convBrailleToRawUnicodeValue
+	local convRawUnicodeValueToUnicodeTests = {
+		{"1st", convBrailleToRawUnicodeValue, {0x3333}, 0x28FF}
+		,{"2nd", convBrailleToRawUnicodeValue, {0x1110}, 0x2807}
+		,{"3rd", convBrailleToRawUnicodeValue, {0x1111}, 0x2847}
+		,{"4th", convBrailleToRawUnicodeValue, {0x1023}, 0x28E1}
+		,{"5th", convBrailleToRawUnicodeValue, {0x0223}, 0x28F0}
+	}
+
+	if not doTests("convRawUnicodeValueToUnicode", convRawUnicodeValueToUnicodeTests) then
+		return
+	end
+
+	function bitmapDrawBorder(width, height, borderThickness)
+		local bmp = Bitmap:new(width, height)
+
+		bmp:drawBorder(borderThickness)
+
+		return bmp:getBuffer()
+	end
+
+	-- Bitmap:drawBorder
+	local BitmapDrawBorderTests = {
+		{"four by four", bitmapDrawBorder, {4, 4, 1}, [[
+⣏⣹
+]]}
+		,{"eight by eight", bitmapDrawBorder, {8, 8, 1}, [[
+⡏⠉⠉⢹
+⣇⣀⣀⣸
+]]}
+		-- NOTE that empty spaces are unicode empty braille, not normal spaces
+		-- we should probably fix that
+		,{"twenty by twenty", bitmapDrawBorder, {20, 20, 1}, [[
+⡏⠉⠉⠉⠉⠉⠉⠉⠉⢹
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸
+⣇⣀⣀⣀⣀⣀⣀⣀⣀⣸
+]]}
+	}
+
+	if not doTests("BitmapDrawBorder", BitmapDrawBorderTests) then
+		return
+	end
+end
+
+function Poll()
+	stopGame()
+end
